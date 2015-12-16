9778f6159f9378a6b01b7b3cc57dbe0ddb2e3830
Job Cuppen <jobcuppen@live.nl>
2015-12-16 15:02:26 +0100

    dit is de laatste zip van blackboard

--- Machiavelli/Machiavelli/Player.cpp
+++ Machiavelli/Machiavelli/Player.cpp
@@ -1,3 +1,10 @@
-#include "Player.hpp"
+//
+//  Player.cpp
+//  socketexample
+//
+//  Created by Bob Polis on 23-11-15.
+//  Copyright © 2015 Avans Hogeschool, 's-Hertogenbosch. All rights reserved.
+//
 
+#include "Player.hpp"
 using namespace std;
